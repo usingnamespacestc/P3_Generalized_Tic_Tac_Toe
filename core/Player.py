32e7ed0653d234268026453d8504519535f8fcaa
usingnamespacestc <usingnamespacestc@gmail.com>
2022-04-18 22:53:53 -0400

    generateScoreModel finished

--- core/Player.py
+++ core/Player.py
@@ -8,11 +8,15 @@ class Player:
         self.started = True
         if self.color == "black":
             self.rivalColor = "white"
+            self.number = 1
+            self.rivalNumber = 2
             # if continuing an existing game, set the boardMap before initializing players
             if 1 not in boardMap and 2 not in boardMap:
                 self.started = False  # then should place first
         else:
             self.rivalColor = "black"
+            self.number = 2
+            self.rivalNumber = 1
         self.boardSize = boardSize
         self.boardMap = boardMap
         self.target = target
@@ -54,19 +58,26 @@ class Player:
             # level  5
             # living 4
             [10000, [0, 1, 1, 1, 1, 0], True],
+            # level6
             # any    5
             [100000, [1, 1, 1, 1, 1], True]
 
             # I haven't put "mark" such as [0, 1, 1, 0, 1, 0] into consideration yet
-            # adding them may cause some unknown problems
+            # adding them may cause some unknown problems due to the increasing complexity
             # the value of this type is that its potential of becoming a living 4
             # maybe put them into consideration can help reduce the layer of searching
         ]
         """
-        self.scoreModel.append([1, [2, 1, 0], False])
-        for level in range(1, self.target):
+        self.scoreModel.append([1, [self.rivalNumber, self.number, 0], False])
+        for level in range(1, self.target - 1):
             score = int(math.pow(10, level))
-            self.scoreModel.append([score])
+            pattern1 = [0] + [self.number for _ in range(0, level)] + [0]  # like [0, 1, 1, 1, 1, 1, 0]
+            pattern2 = [self.rivalNumber] + [self.number for _ in range(0, level + 1)] + [0]  # like [2, 1, 1, 1, 1, 0]
+            self.scoreModel.append([score, pattern1, True])
+            self.scoreModel.append([score, pattern2, False])
+        self.scoreModel.append([int(math.pow(10, self.target - 1)),
+                                [0] + [self.number for _ in range(0, self.target - 1)] + [0], True])
+        self.scoreModel.append([int(math.pow(10, self.target)), [self.number for _ in range(0, self.target)], True])
 
     def evaluate(self, position):
         """
@@ -83,6 +94,7 @@ class Player:
             if self.testCount < len(self.testSteps):
                 self.testCount += 1
                 return self.testSteps[self.testCount - 1]
+
         # the whole board is empty and this player is the first one
         if not self.started:
             self.started = True
@@ -96,4 +108,5 @@ class Player:
 
 
 if __name__ == '__main__':
-    testPlayer = Player("black", 3, 3, [0 for _ in range(0, 9)])
+    testPlayer = Player(color="black", boardSize=3, target=10, boardMap=[0 for _ in range(0, 9)])
+    print("pause")
